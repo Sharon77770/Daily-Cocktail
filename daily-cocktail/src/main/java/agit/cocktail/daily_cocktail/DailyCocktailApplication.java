0065b3ab554586ae35615df5b949e7d382519d6a
주민재 <john_joo7@naver.com>
2024-11-04 10:25:40 +0900

    setup

---
+++ daily-cocktail/src/main/java/agit/cocktail/daily_cocktail/DailyCocktailApplication.java
@@ -0,0 +1,13 @@
+package agit.cocktail.daily_cocktail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCocktailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyCocktailApplication.class, args);
+	}
+
+}
---
+++ daily-cocktail/src/test/java/agit/cocktail/daily_cocktail/DailyCocktailApplicationTests.java
@@ -0,0 +1,13 @@
+package agit.cocktail.daily_cocktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCocktailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
